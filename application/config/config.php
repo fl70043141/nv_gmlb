975646b79aa1e9eacc3c747f601b99b1bbcf588c
Fahry Lafir <fahrydgt@gmail.com>
2019-01-30 09:29:24 +0530

    stpgmlb

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] =  "http://".$_SERVER['HTTP_HOST'].'/gem_lab_system/';
+$config['base_url'] =  "http://".$_SERVER['HTTP_HOST'].'/nveloop_gemlab/';
 date_default_timezone_set('Asia/Colombo');
 /*
 |--------------------------------------------------------------------------
